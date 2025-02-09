fd0d46e919dba4bfb88d0917c7df3610df4b0ad4
Patbox <39821509+Patbox@users.noreply.github.com>
2025-02-09 17:53:18 +0100

    Throw exception in default interface implementation, fix license missing

--- fabric-recipe-api-v1/src/main/java/net/fabricmc/fabric/api/recipe/v1/FabricServerRecipeManager.java
+++ fabric-recipe-api-v1/src/main/java/net/fabricmc/fabric/api/recipe/v1/FabricServerRecipeManager.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.api.recipe.v1;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Stream;
 
 import net.minecraft.recipe.Recipe;
@@ -24,13 +39,13 @@ public interface FabricServerRecipeManager {
 	 * @return the stream of matching recipes
 	 */
 	default <I extends RecipeInput, T extends Recipe<I>> Stream<RecipeEntry<T>> getAllMatches(RecipeType<T> type, I input, World world) {
-		return Stream.of();
+		throw new AssertionError("Implemented in Mixin");
 	}
 
 	/**
 	 * @return the collection of recipe entries of given type
 	 */
 	default <I extends RecipeInput, T extends Recipe<I>> Collection<RecipeEntry<T>> getAllOfType(RecipeType<T> type) {
-		return List.of();
+		throw new AssertionError("Implemented in Mixin");
 	}
 }
