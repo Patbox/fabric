233ecb21d7c97cdfdc5496d5374dc5cdf79f904f
DennisOchulor <113277083+DennisOchulor@users.noreply.github.com>
2026-07-01 20:44:06 +0800

    [26.3] Complete todo comments for AttachmentChange (#5430)
    * complete todo comments for AttachmentChange 26.2
    
    * remove else
    
    ---------
    
    Co-authored-by: modmuss <modmuss50@gmail.com>

--- fabric-data-attachment-api-v1/src/main/java/net/fabricmc/fabric/impl/attachment/sync/AttachmentChange.java
+++ fabric-data-attachment-api-v1/src/main/java/net/fabricmc/fabric/impl/attachment/sync/AttachmentChange.java
@@ -33,7 +33,6 @@ import net.minecraft.world.level.Level;
 
 import net.fabricmc.fabric.api.attachment.v1.AttachmentTarget;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
-import net.fabricmc.fabric.api.networking.v1.FriendlyByteBufs;
 import net.fabricmc.fabric.impl.attachment.AttachmentRegistryImpl;
 import net.fabricmc.fabric.impl.attachment.AttachmentTypeImpl;
 
@@ -50,29 +49,19 @@ public record AttachmentChange(AttachmentTargetInfo<?> targetInfo, AttachmentTyp
 		Identifier.STREAM_CODEC.encode(buf, Objects.requireNonNull(this.type.identifier()));
 
 		if (this.value == null) {
-			// Todo: Legacy Format, writeVarInt should be removed for 26.2
-			buf.writeVarInt(1);
 			buf.writeBoolean(false);
 			return;
 		}
+
+		buf.writeBoolean(true);
 
 		//noinspection unchecked
 		StreamCodec<? super RegistryFriendlyByteBuf, Object> codec = (StreamCodec<? super RegistryFriendlyByteBuf, Object>) type.streamCodec();
 		Objects.requireNonNull(codec, "attachment stream codec cannot be null");
 
-		// Todo: Legacy Format, buf2 should be removed and replaced with buf for 26.2
-		// Todo: int currentLength = buf.readableBytes();
-		RegistryFriendlyByteBuf buf2 = new RegistryFriendlyByteBuf(FriendlyByteBufs.create(), buf.registryAccess());
-
-		buf2.writeBoolean(true);
-		codec.encode(buf2, value);
-
-		// Todo: Legacy Format, `buf2.readableBytes()` should be replaced with `buf.readableBytes() - currentLength` for 26.2
-		validateMaxPayloadSize(type, buf2.readableBytes());
-
-		// Todo: Legacy Format, remove buf2 for 26.2
-		buf.writeVarInt(buf2.readableBytes());
-		buf.writeBytes(buf2);
+		int metadataLength = buf.readableBytes();
+		codec.encode(buf, value);
+		validateMaxPayloadSize(type, buf.readableBytes() - metadataLength);
 	}
 
 	private static AttachmentChange decodePacket(RegistryFriendlyByteBuf buf) {
@@ -80,13 +69,12 @@ public record AttachmentChange(AttachmentTargetInfo<?> targetInfo, AttachmentTyp
 		AttachmentTypeImpl<?> type = (AttachmentTypeImpl<?>) AttachmentRegistryImpl.get(Identifier.STREAM_CODEC.decode(buf));
 		Objects.requireNonNull(type, "attachment type cannot be null");
 
-		// Todo: Legacy Format, readVarInt should be replaced with buf.readableBytes() for 26.2
-		int size = buf.readVarInt();
-		validateMaxPayloadSize(type, size);
-
 		if (!buf.readBoolean()) {
 			return new AttachmentChange(target, type, null);
 		}
+
+		int size = buf.readableBytes();
+		validateMaxPayloadSize(type, size);
 
 		//noinspection unchecked
 		StreamCodec<? super RegistryFriendlyByteBuf, Object> codec = (StreamCodec<? super RegistryFriendlyByteBuf, Object>) type.streamCodec();
